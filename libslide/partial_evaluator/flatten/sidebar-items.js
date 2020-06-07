1e161b50e1c45aac2d866e20810c9567b7eb7138
hafiz <deploy@travis-ci.org>
2020-06-07 20:45:06 +0000

    Deploy slide to github.com/ayazhafiz/slide.git:gh-pages

---
+++ libslide/partial_evaluator/flatten/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["flatten_add_or_sub","Flattens an addition or subtraction, folding constants and like terms as far as possible. The flattened expression is always normalized to an addition."],["flatten_expr","Attempts to flatten an expression, folding constant expressions and like terms."]]});
