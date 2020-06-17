4804d5fb10822dba3c34e98a235267978283e4bc
hafiz <deploy@travis-ci.org>
2020-06-17 06:00:50 +0000

    Deploy slide to github.com/ayazhafiz/slide.git:gh-pages

--- libslide/parser/sidebar-items.js
+++ libslide/parser/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["expression_parser",""],["expression_pattern_parser",""],["test_utils",""]],"trait":[["Parser",""]]});
+initSidebarItems({"fn":[["unclosed_delimiter","Returns a diagnostic for an unclosed delimiter."]],"mod":[["expression_parser",""],["expression_pattern_parser",""],["test_utils",""]],"trait":[["Parser",""]]});
